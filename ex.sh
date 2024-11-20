81dda41aaf096f84203cb9a1cb2e232f402d08c2
adapreda <adapreda2005@gmail.com>
2024-11-20 09:59:06 +0000

    versiune modificata a verificarii ip-urilor

--- ex.sh
+++ ex.sh
@@ -8,10 +8,12 @@ cat /etc/hosts | while read -r line; do
 ip=$(echo "$line" | cut -d ' ' -f 1)
 name=$(echo "$line" | cut -d ' ' -f 2)
 
-resolved_ip=$(nslookup "$name" 2>/dev/null | grep 'Address:' | tail -n 1 | awk '{print $2}')
+resolved_ip=$(nslookup "$name" 2>/dev/null | grep 'Address:' | cut -d ':' -f 2 | grep $ip | cut -b 2-)
 
 if [[ "$resolved_ip" != "$ip" &&  "$resolved_ip" != "" ]]; then
 	echo "Bogus IP for $name in /etc/hosts! Expected: $ip, Got: $resolved_ip"
+elif [[ "$resolved_ip" == "$ip" && "$resolved_ip" != "" ]]; then
+	echo "Adresa $resolved_ip este corecta!"
 fi
 
 done
